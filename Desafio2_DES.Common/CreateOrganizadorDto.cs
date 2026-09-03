9fa19f055d1f64c54affa6f1417d2e21cf4ae87b
Melany.Bojorquez <bojorquezfabiola8@gmail.com>
2026-09-02 20:16:12 -0600

    Agregar DTOs y validaciones de Participante y Organizador

---
+++ Desafio2_DES.Common/CreateOrganizadorDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Desafio2_DES.Common;
+
+public class CreateOrganizadorDto
+{
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [MinLength(3), MaxLength(50)]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El cargo es obligatorio")]
+    [MinLength(3), MaxLength(50)]
+    public string Cargo { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El evento asociado es obligatorio")]
+    public int IdEvento { get; set; }
+}
---
+++ Desafio2_DES.Common/OrganizadorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio2_DES.Common;
+
+public class OrganizadorDto
+{
+    public int IdOrganizador { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Cargo { get; set; } = string.Empty;
+    public int IdEvento { get; set; }
+}
